1d49ce9552979e27c93561e82febf3db8c66906f
Mete Atamel <atamel@google.com>
2024-01-15 15:46:46 +0000

    Added tests for exists functionality

--- ToDoApiTest/TodoItemsControllerTests.cs
+++ ToDoApiTest/TodoItemsControllerTests.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TodoApi.Controllers;
+using TodoApi.Models;
 using Xunit;
 
 namespace TodoApi.Tests;
@@ -16,5 +19,44 @@ public class TodoItemsControllerTests
 
         // Assert
         Assert.NotNull(result);
+    }
+
+    [Fact]
+    public async Task HeadTodoItem_ReturnsNoContent_WhenItemExists()
+    {
+        // Arrange
+        var options = new DbContextOptionsBuilder<TodoContext>()
+            .UseInMemoryDatabase(databaseName: "TodoItems")
+            .Options;
+        var context = new TodoContext(options);
+        var controller = new TodoItemsController(context);
+        var todoItem = new TodoItem { Id = 1, Name = "Item 1", IsComplete = false };
+        context.TodoItems.Add(todoItem);
+        await context.SaveChangesAsync();
+        var id = 1;
+
+        // Act
+        var result = await controller.HeadTodoItem(id);
+
+        // Assert
+        Assert.IsType<NoContentResult>(result);
+    }
+
+    [Fact]
+    public async Task HeadTodoItem_ReturnsNotFound_WhenItemDoesNotExist()
+    {
+        // Arrange
+        var options = new DbContextOptionsBuilder<TodoContext>()
+            .UseInMemoryDatabase(databaseName: "TodoItems")
+            .Options;
+        var context = new TodoContext(options);
+        var controller = new TodoItemsController(context);
+        var id = 2;
+
+        // Act
+        var result = await controller.HeadTodoItem(id);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(result);
     }
 }
